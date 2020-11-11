706d6f7e5a44aeba7914c5609395e69ba0b64def
Joel Rosenblatt <7117670+jo-rs@users.noreply.github.com>
2020-11-11 19:32:30 +0100

    Fix query

--- src/pages/auctions/[id].tsx
+++ src/pages/auctions/[id].tsx
@@ -9,96 +9,104 @@ export default function Auction(props) {
 }
 
 export const getStaticProps = async ({ params: { id } }) => {
-  const res = await metaphysicsFetcher(`
-    query GetSaleQuery {
-      sale(id: ${id}) {
-        internalID
-        slug
-        name
-        description
-        currency
-        startAt
-        liveStartAt
-        endAt
-        isClosed
-        registrationEndsAt
-        isLiveOpen
-        buyersPremium {
-          amount
-          percent
-        }
-        saleArtworksConnection {
-          edges {
-            node {
-              internalID
-              slug
-              lotLabel
-              increments {
-                cents
-                display
-              }
-              lowEstimate {
-                cents
-              }
-              highEstimate {
-                cents
-              }
-              symbol
-              currency
-              estimate
-              reserveStatus
-              reserve {
-                cents
-              }
-              artwork {
-                id
-                title
-                date
-                medium
-                category
-                description
-                dimensions {
-                  in
-                  cm
+  try {
+    const res = await metaphysicsFetcher(`
+      query GetSaleQuery {
+        sale(id: "${id}") {
+          internalID
+          slug
+          name
+          description
+          currency
+          startAt
+          liveStartAt
+          endAt
+          isClosed
+          registrationEndsAt
+          isLiveOpen
+          buyersPremium {
+            amount
+            percent
+          }
+          saleArtworksConnection {
+            edges {
+              node {
+                internalID
+                slug
+                lotLabel
+                increments {
+                  cents
+                  display
                 }
-                editionOf
-                partner(shallow: true) {
+                lowEstimate {
+                  cents
+                }
+                highEstimate {
+                  cents
+                }
+                symbol
+                currency
+                estimate
+                reserveStatus
+                reserve {
+                  cents
+                }
+                artwork {
                   id
-                  name
-                  profile {
-                    icon {
-                      url
+                  title
+                  date
+                  medium
+                  category
+                  description
+                  dimensions {
+                    in
+                    cm
+                  }
+                  editionOf
+                  partner(shallow: true) {
+                    id
+                    name
+                    profile {
+                      icon {
+                        url
+                      }
                     }
                   }
-                }
-                artist(shallow: true) {
-                  name
-                  id
-                }
-                image {
-                  width
-                  height
-                  url(version: "large")
+                  artist(shallow: true) {
+                    name
+                    id
+                  }
+                  image {
+                    width
+                    height
+                    url(version: "large")
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  `)
-  // const auction = await res.json()
-  return {
-    props: {
-      res,
+    `)
+    return {
+      props: {
+        sale: res.sale,
 
-      // auction,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every second
-    revalidate: 60, // In seconds
+        // auction,
+      },
+      // Next.js will attempt to re-generate the page:
+      // - When a request comes in
+      // - At most once every second
+      revalidate: 60, // In seconds
+      // notFound: !res.status(200)
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      notFound: true
+    }
   }
+  // const auction = await res.json()
 }
 
 export async function getStaticPaths() {
